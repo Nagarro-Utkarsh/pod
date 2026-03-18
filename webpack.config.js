64916856d117cd4b4e825b63a55526ce99ea820c
Utkarsh Mahajan <utkarsh.mahajan@Utkarshs-MacBook-Pro.local>
2026-03-18 15:39:41 +0530

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,38 @@ const { container } = require("webpack");
 const srcDir = path.resolve(__dirname, "src");
 
 const exposes = readdirSync(srcDir, { withFileTypes: true })
-  .filter((d) => d.isDirectory())
-  .reduce((acc, dir) => {
-    const tsx = path.resolve(srcDir, dir.name, `${dir.name}.tsx`);
-    if (existsSync(tsx)) acc[`./${dir.name}`] = `./src/${dir.name}/${dir.name}.tsx`;
-    return acc;
-  }, {});
+    .filter((d) => d.isDirectory())
+    .reduce((acc, dir) => {
+      const tsx = path.resolve(srcDir, dir.name, `${dir.name}.tsx`);
+      if (existsSync(tsx)) {
+        acc[`./${dir.name}`] = `./src/${dir.name}/${dir.name}.tsx`;
+      }
+      return acc;
+    }, {});
 
 module.exports = (_, argv) => ({
   entry: "./src/main.ts",
+
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
     publicPath: "auto",
     clean: true,
+
+    // 🔴 REQUIRED for Module Federation
+    library: {
+      type: "var",
+      name: "pod",
+    },
   },
+
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     alias: {
       "@": srcDir,
     },
   },
+
   module: {
     rules: [
       {
@@ -43,28 +54,49 @@ module.exports = (_, argv) => ({
           "style-loader",
           {
             loader: "css-loader",
-            options: { modules: { localIdentName: "[name]__[local]--[hash:base64:5]" } },
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+            },
           },
           "sass-loader",
         ],
       },
     ],
   },
+
   plugins: [
     new container.ModuleFederationPlugin({
       name: "pod",
       filename: "remoteEntry.js",
+
       exposes,
+
       shared: {
-        react: { singleton: true, eager: true, requiredVersion: false },
-        "react-dom": { singleton: true, eager: true, requiredVersion: false },
+        react: {
+          singleton: true,
+          requiredVersion: false,
+          eager: false, // 🔴 IMPORTANT: remove eager
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: false,
+          eager: false,
+        },
       },
     }),
-    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
   ],
+
   devServer: {
     port: 3001,
     hot: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
 });
